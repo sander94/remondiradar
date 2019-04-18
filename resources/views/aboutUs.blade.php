566da2e07a94c0c96c6d9d05b4d6e169a381f4bf
sander94 <sanderjyrgens@gmail.com>
2019-04-18 14:38:20 +0300

    about us

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -5,7 +5,7 @@
 <div class="content pricerequest">
 	<div class="content-box">
 		<div class="row">
-			<div class="col-md-8">
+			<div class="col-md-5">
 				<h1 class="heading" style="margin-bottom: 10px;"><strong>Mis on Remondiradar?</strong></h1>
 				<style>
 				p strong { font-weight: 700; }
@@ -28,9 +28,9 @@
 				<div style="height: 100px;"></div>
 			</div>
 
-			<div class="col-md-4">
+			<div class="col-md-7">
 
-				<div style="height: 270px; width: 100%; background-color: orange;"> </div>
+				<div style="height: 270px; width: 100%; background-color: orange;"><img src="{{ url('images/web/about_us.jpg') }}" style="width: 100%;"></div>
 
 			</div>
 
@@ -38,38 +38,83 @@
 		</div>
 
 
+<style type="text/css">
+	.explainerBox {
+		border: 1px solid #e6e6e6;
+		padding-left: 25px;
+		padding-right: 25px;
+		padding: 25px;
+		min-height: 250px;
+		background-color: #72cbff;
+	} 
+
+	.explainerBox-white {
+		background-color: #FFFFFF;
+	}
+
+	.explainerBox p {
+		font-size: 20px;
+		color: black;
+	}
+
+	.explainerBox h4 {
+		margin-bottom: 30px;
+	}
+
+	@media screen and (max-width: 1300px) {
+		.explainerBox {
+			height: 350px;
+		}
+		.explainerBox h4 {
+			font-size: 16px;
+		}
+	}
+
+		@media screen and (max-width: 790px) {
+		.explainerBox {
+			height: 450px;
+		}
+	}
+
+		@media screen and (max-width: 767px) {
+		.explainerBox {
+			height: auto;
+			min-height: auto;
+		}
+	}
+</style>
 
 
 
 
-		<div class="row">
+		<div class="row" style="margin-top: 50px;">
 
 			<div class="col-md-12">
-				<h2 class="text-center" style="font-weight: 700;">Omad töökoda? Liitu tasuta!</h2>
+				<h2 class="text-center" style="font-weight: 700;">Omad töökoda? <a href="{{ url('register') }}" style="color: #175df6;">Liitu tasuta!</a></h2>
 					
 					<div class="row" style="margin-top: 50px;">
 
 						<div class="col-md-4 text-center">
-
-							<h4 style="color: orange; font-weight: 700;">PIIRAMATU ARV TÖÖKODASID</h4>
-							<p>Kui Sul on mitu töökoda, saad neid kõiki hallata ühelt kontolt. Töökodasid saab lisada piiramatul hulgal.<br> Ei mingeid lisatasusid.</p>
-
+							<div class="explainerBox">
+								<h4 style="color: orange; font-weight: 700; color: #FFFFFF;">TÖÖKOJA HALDUS</h4>
+								<p>Kui Sul on mitu töökoda, saad neid kõiki hallata ühelt kontolt. Töökodasid saab lisada piiramatul hulgal. Ei mingeid lisatasusid.</p>
+							</div>
 						</div>
 
 						<div class="col-md-4 text-center">
-
-							<h4 style="color: orange; font-weight: 700;">HINNAPÄRINGUD</h4>
-							<p>Teenuse otsija saab ühe sisestamisega saata hinnapäringu kõigile kohalikele töökodadele. Saabunud päringud on nähtavad
-							Sinu ettevõtte halduskontol, kus saad teha hinnapakkumise ja pakkuda esimese vaba aja oma töökotta.</p>
-
+							<div class="explainerBox">
+								<h4 style="color: orange; font-weight: 700; color: #FFFFFF;">HINNAPÄRINGUD</h4>
+								<p>Teenuse otsija saab ühe sisestamisega saata hinnapäringu kõigile kohalikele töökodadele. Saabunud päringud on nähtavad
+								Sinu ettevõtte halduskontol, kus saad teha hinnapakkumise ja pakkuda esimese vaba aja oma töökotta.</p>
+							</div>
 						</div>
 
 
 						<div class="col-md-4 text-center">
-
-							<h4 style="color: orange; font-weight: 700;">TÖÖKOJA VISIITKAART</h4>
-							<p>Igal töökojal on oma visiitkaart - leht, kus kogu info koos asukohakaardiga on esinduslikult välja toodud. Kuna üle poolte teenuseotsijate kasutab mobiilseadmeid, on väga oluline omada kaunist mobiilisõbralikku lehte.</p>
-
+							<div class="explainerBox">
+								<h4 style="color: orange; font-weight: 700; color: #FFFFFF;">TÖÖKOJA VISIITKAART</h4>
+								<p>Igal töökojal on oma visiitkaart - leht, kus kogu info koos asukohakaardiga on esinduslikult välja toodud. Kuna üle poolte teenuseotsijate kasutab mobiilseadmeid, on väga oluline omada kaunist mobiilisõbralikku lehte.</p>
+							</div>
 						</div>
 
 
@@ -83,6 +128,9 @@
 		</div>
 
 
+
+
+
 <div style="height: 100px;"> </div>
 
 
@@ -94,25 +142,25 @@
 					<div class="row" style="margin-top: 50px;">
 
 						<div class="col-md-4 text-center">
-
-							<h4 style="color: orange; font-weight: 700;">ARVETE GENEREERIMINE</h4>
-							<p>Võimalus genereerida PDF arveid ning saata need otse kliendile. Kõik arved talletatakse meie andmebaasis - hiljem saate sorteerida ja näha kõiki saadetud arveid </p>
-
+							<div class="explainerBox explainerBox-white">
+								<h4 style="color: orange; font-weight: 700;">ARVETE GENEREERIMINE</h4>
+								<p>Võimalus genereerida PDF arveid ning saata need otse kliendile. Kõik arved talletatakse meie andmebaasis - hiljem saate sorteerida ja näha kõiki saadetud arveid </p>
+							</div>
 						</div>
 
 						<div class="col-md-4 text-center">
-
-							<h4 style="color: orange; font-weight: 700;">LIVE-CHAT</h4>
-							<p>Kasutaja, kes vaatab teie töökoja lehte, saab otse vestelda teie konto haldajaga. Live-chat toimub läbi haldusliidese.</p>
-
+							<div class="explainerBox explainerBox-white">
+								<h4 style="color: orange; font-weight: 700;">LIVE-CHAT</h4>
+								<p>Kasutaja, kes vaatab teie töökoja lehte, saab otse vestelda teie konto haldajaga. Live-chat toimub läbi haldusliidese.</p>
+							</div>
 						</div>
 
 
 						<div class="col-md-4 text-center">
-
-							<h4 style="color: orange; font-weight: 700;">TAGASISIDE</h4>
-							<p>Läbi halduskonto on igale kliendile võimalik saata tagasiside vorm, kus antakse hinnang ja kommentaar teenuse kvaliteedile. Kõrgem reiting tagab suurema huvi töökoja vastu.</p>
-
+							<div class="explainerBox explainerBox-white">
+								<h4 style="color: orange; font-weight: 700;">TAGASISIDE</h4>
+								<p>Läbi halduskonto on igale kliendile võimalik saata tagasiside vorm, kus antakse hinnang ja kommentaar teenuse kvaliteedile. Kõrgem reiting tagab suurema huvi töökoja vastu.</p>
+							</div>
 						</div>
 
 
