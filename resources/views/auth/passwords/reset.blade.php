152ab59c17697b9bc8956fc311417dde50538688
sander94 <sanderjyrgens@gmail.com>
2020-09-30 16:20:31 +0300

    Update reset.blade.php

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -14,7 +14,7 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-maili aadress') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">E-maili aadress</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus>
@@ -28,7 +28,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Parool') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Parool</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
