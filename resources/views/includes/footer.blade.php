56f098905f5344392596a7a8301bf1e99b07a19d
sander94 <sanderjyrgens@gmail.com>
2020-10-05 17:23:34 +0300

    nothing

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -4,7 +4,6 @@
 			<div class="col-12 col-sm-12 col-md-4">
 				<h3>REMONDIRADAR</h3>
 				<p><i class="fas fa-envelope fa-fw"> </i> info@remondiradar.ee</p>
-				<p><i class="fas fa-phone fa-fw fa-flip-horizontal"> </i> +372 5825 4272</p>
 			</div>
 
 			<div class="col-12 col-sm-12 col-md-4">
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,8 +13,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-          integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
