5e8697c89deb484a557ebf5800619ea155d5928a
oleghalin <oleghalinua@gmail.com>
2021-02-18 12:09:28 +0200

    Publish media

---
+++ app/UrlGenerator/LocalUrlGenerator.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\UrlGenerator;
+
+
+use Illuminate\Support\Str;
+use Spatie\MediaLibrary\Exceptions\UrlCannotBeDetermined;
+use Spatie\MediaLibrary\UrlGenerator\LocalUrlGenerator as BaseLocalUrlGenerator;
+
+class LocalUrlGenerator extends BaseLocalUrlGenerator
+{
+
+    protected function getBaseMediaDirectoryUrl(): string
+    {
+        if ($diskUrl = $this->config->get("filesystems.disks.{$this->media->disk}.url")) {
+            return $diskUrl;
+        }
+
+        if (! Str::startsWith($this->getStoragePath(), public_path())) {
+            throw UrlCannotBeDetermined::mediaNotPubliclyAvailable($this->getStoragePath(), public_path());
+        }
+
+        return $this->getBaseMediaDirectory();
+    }
+}
